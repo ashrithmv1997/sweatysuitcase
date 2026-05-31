1031d52f3788d1ed82fd0fac4c4bca30a11b0ff9
ashrithmv1997 <5zs2c5gc4m@privaterelay.appleid.com>
2026-05-31 11:06:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ function scrollToSection() {
     block: 'start'
   });
 }
+document.addEventListener("DOMContentLoaded", () => {
+
+  const btn =
+    document.getElementById("remaniWidgetButton");
+
+  const widget =
+    document.getElementById("remaniWidget");
+
+  if (!btn || !widget) {
+    console.error("Remani widget elements not found");
+    return;
+  }
+
+  btn.addEventListener("click", () => {
+
+    if (widget.style.display === "block") {
+      widget.style.display = "none";
+    } else {
+      widget.style.display = "block";
+    }
+
+  });
+
+});
